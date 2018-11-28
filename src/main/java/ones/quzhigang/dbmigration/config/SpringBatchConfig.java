2105575b222c4145744842d5a2906109c9c50b86
屈志刚 <598700560@qq.com>
2018-11-28 16:43:21 +0800

    Initial commit

---
+++ src/main/java/ones/quzhigang/dbmigration/config/SpringBatchConfig.java
@@ -0,0 +1,20 @@
+package ones.quzhigang.dbmigration.config;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Copyright (C), 2018, 上海米袋融资有限公司
+ * @ProjectName: new_gold_midai
+ * @FileName: SpringBatchConfig
+ * @Author: 屈志刚
+ * @Date: 2018/11/28 0028 16:32
+ * @Description:
+ * @email ==>> jing9241120@sina.com
+ * @History: <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+@Configuration
+@EnableBatchProcessing
+public class SpringBatchConfig {
+}
---
+++ src/main/java/ones/quzhigang/dbmigration/support/Test.java
@@ -0,0 +1,15 @@
+package ones.quzhigang.dbmigration.support;
+
+/**
+ * @Copyright (C), 2018, 上海米袋融资有限公司
+ * @ProjectName: new_gold_midai
+ * @FileName: Test
+ * @Author: 屈志刚
+ * @Date: 2018/11/28 0028 16:24
+ * @Description:
+ * @email ==>> jing9241120@sina.com
+ * @History: <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+public class Test {
+}
